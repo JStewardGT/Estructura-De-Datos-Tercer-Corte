50c9a19dd83b136b5afc408dbbafef66aab5b004
Johan Steward Galeano Torres <44073891+JStewardGT@users.noreply.github.com>
2019-04-25 23:03:10 -0500

    create

---
+++ src/Guia4_ListasCirculares/Actividad1_MultiplosDe3de9a21.java
@@ -0,0 +1,42 @@
+package Guia4_ListasCirculares;
+
+/**
+ *
+ * @author stewa
+ */
+public class Actividad1_MultiplosDe3de9a21 {
+
+    public static void main(String[] args) {
+        Nodo Cabeza = new Nodo();
+        Nodo recorrer = Cabeza;
+        int contador = 9;
+        while (contador <= 21) {
+            recorrer.info = contador;
+            if (contador < 21) {
+                recorrer.sig = new Nodo();
+                recorrer = recorrer.sig;
+            }
+            contador += 3;
+        }
+        recorrer.sig = Cabeza;
+        recorrer = Cabeza;
+        int i = 0;
+        while (i < 2) {
+            if (recorrer.sig == Cabeza) {
+                i++;
+            }
+            System.out.println(recorrer.info);
+            recorrer = recorrer.sig;
+        }
+        
+        Nodo recorrido = Cabeza.sig;
+        while (recorrido != Cabeza) {
+                System.out.println(recorrido.info);
+                recorrido = recorrido.sig;
+            }
+       System.out.println(recorrido.info);
+
+
+    }
+
+}
