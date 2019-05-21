b635d2c9e1146f84757e42dcbdbdf204246b840a
ShenGilon <47929087+ShenGilon@users.noreply.github.com>
2019-05-20 21:13:17 -0500

    punto 1 del quinto taller

--- src/Taller5Actividad1_Atender40a50/Main.java
+++ src/Taller5Actividad1_Atender40a50/Main.java
@@ -1,6 +1,6 @@
 /*
  * Realice un programa de colas, donde vayan ingresando automáticamente los 
- * clientes numerados  del 40 al 51, en el cual ingresan  2 clientes  y se 
+ * clientes numerados  del 40 al 51, en el cual ingresan 2 clientes  y se 
  * atiende 1. Verificar quienes quedaron en la cola. 
  */
 package Taller5Actividad1_Atender40a50;
@@ -13,26 +13,20 @@ public class Main {
 
     public static void main(String[] args) {
         Cola cola = new Cola();
-        int opcion = -1;
-        while (opcion != 4) {
-            System.out.println("MENU:");
-            System.out.println("1. agregar");
-            System.out.println("2. atender");
-            System.out.println("3. listar");
-            System.out.println("4. salir");
-            opcion = Leer.datoInt();
-            if (opcion == 1) {
-                Informacion info = new Informacion();
-                System.out.println("digite el dato:");
-                info.dato = Leer.dato();
-                cola.agregar(info);
-            } else if (opcion == 2) {
-                Informacion info = cola.atender();
-                System.out.println("dato atendido : " + info.dato);
-            } else if (opcion == 3) {
-                cola.listar();
-            }
+        for (int i = 40; i <= 51; i++) {
+            Informacion info = new Informacion();
+            info.dato = "Cliente " + i;
+            cola.agregar(info);
         }
+        for (int i = 0; i <= 1; i++) {
+            Informacion info = new Informacion();
+            System.out.println("digite el cliente " + (i + 1));
+            info.dato = Leer.dato();
+            cola.agregar(info);
+        }
+        Informacion info = cola.atender();
+        cola.listar();
+        
         System.out.println("terminado");
     }
 
